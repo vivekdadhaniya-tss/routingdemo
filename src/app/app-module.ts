669ab7f81384dde9060929c102cb4bb3780710fb
vivekdadhaniya-tss <vivek.dadhaniya@tssconsultancy.com>
2026-04-16 16:19:28 +0530

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './navbar/navbar/navbar';
+
+@NgModule({
+  declarations: [
+    App,
+    Navbar
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
